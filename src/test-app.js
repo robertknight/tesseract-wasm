d90c7b9c502409dc31a6500403da48718e923990
Robert Knight <robertknight@gmail.com>
2022-05-17 13:58:36 +0100

    Update code for test app to use updated C++ APIs for text extraction

--- src/test-app.js
+++ src/test-app.js
@@ -18,29 +18,74 @@ async function fetchImage() {
   return context.getImageData(0, 0, bitmap.width, bitmap.height);
 }
 
-Promise.all([initOCRLib(), fetchOCRModel(), fetchImage()])
-  .then(async ([ocrLib, ocrModel, imageData]) => {
-    console.log("OCR model size", ocrModel.length);
-    console.log("Image size", imageData.width, imageData.height);
+/**
+ * Create a JS array from a std::vector wrapper created by Embind.
+ */
+function jsArrayFromStdVector(vec) {
+  const size = vec.size();
+  const result = [];
+  for (let i = 0; i < size; i++) {
+    result.push(vec.get(i));
+  }
+  return result;
+}
 
-    const { OCREngine } = ocrLib;
-    const engine = new OCREngine();
-    console.log("Loading OCR model", ocrModel);
-    const modelResult = engine.loadModel(ocrModel);
-    console.log("Model load result...", modelResult);
-
-    console.log("Extracting text");
-    const start = performance.now();
-    const result = engine.extractText(
-      imageData.data,
-      imageData.width,
-      imageData.height,
-      4 /* bytesPerPixel */,
-      imageData.width * 4 /* bytesPerLine */
-    );
+function timeIt(label, callback) {
+  const start = performance.now();
+  const result = callback();
+  if (typeof result?.then === "function") {
+    return result.then((value) => {
+      const end = performance.now();
+      console.log(`${label} took ${end - start}ms`);
+      return value;
+    });
+  } else {
     const end = performance.now();
-    console.log("result", result);
-    console.log("OCR time", end - start);
+    console.log(`${label} took ${end - start}ms`);
+    return result;
+  }
+}
+
+Promise.all([
+  timeIt("initializing OCR library", initOCRLib),
+  timeIt("fetching OCR model", fetchOCRModel),
+  timeIt("fetching image", fetchImage),
+])
+  .then(async ([ocrLib, ocrModel, imageData]) => {
+    const { OCREngine, TextUnit } = ocrLib;
+    const engine = new OCREngine();
+    const modelResult = timeIt("loading model", () =>
+      engine.loadModel(ocrModel)
+    );
+    if (modelResult !== 0) {
+      throw new Error("Failed to load model");
+    }
+
+    let result = timeIt("loading image", () =>
+      engine.loadImage(
+        imageData.data,
+        imageData.width,
+        imageData.height,
+        4 /* bytesPerPixel */,
+        imageData.width * 4 /* bytesPerLine */
+      )
+    );
+    if (result !== 0) {
+      throw new Error("Failed to load image");
+    }
+
+    const textBoxes = timeIt("getting word boxes", () =>
+      jsArrayFromStdVector(engine.getBoundingBoxes(TextUnit.Word))
+    );
+    const textLineBoxes = timeIt("getting line boxes", () =>
+      jsArrayFromStdVector(engine.getBoundingBoxes(TextUnit.Line))
+    );
+    const textLines = timeIt("getting text lines", () =>
+      jsArrayFromStdVector(engine.getText(TextUnit.Line))
+    );
+    const textWords = timeIt("getting text words", () =>
+      jsArrayFromStdVector(engine.getText(TextUnit.Word))
+    );
   })
   .catch((err) => {
     console.error("OCR init failed", err);
