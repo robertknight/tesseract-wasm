c241df9169fbb7c9c35b345751de308b685d0cce
Robert Knight <robertknight@gmail.com>
2022-05-17 13:57:58 +0100

    Expose APIs for getting bounding boxes + text from Tesseract

--- src/lib.cpp
+++ src/lib.cpp
@@ -7,22 +7,61 @@
 #include <string>
 #include <vector>
 
+struct IntRect {
+  int left = 0;
+  int right = 0;
+  int top = 0;
+  int bottom = 0;
+};
+
+struct TextRect {
+  IntRect rect;
+  std::string text;
+};
+
+enum class TextUnit {
+  Word,
+  Line,
+};
+
+template <class T>
+std::unique_ptr<T> unique_from_raw(T* ptr) {
+  return std::unique_ptr<T>(ptr);
+}
+
+std::string string_from_raw(char* ptr) {
+  auto result = std::string(ptr);
+  delete[] ptr;
+  return result;
+}
+
+auto iterator_level_from_unit(TextUnit unit) {
+  tesseract::PageIteratorLevel level;
+  if (unit == TextUnit::Line) {
+    return tesseract::RIL_TEXTLINE;
+  } else if (unit == TextUnit::Word) {
+    return tesseract::RIL_WORD;
+  } else {
+    return tesseract::RIL_SYMBOL;
+  }
+}
+
 using namespace emscripten;
 
 class OCREngine {
  public:
-  OCREngine() : m_tesseract(new tesseract::TessBaseAPI()) {}
+  OCREngine() : tesseract_(new tesseract::TessBaseAPI()) {}
 
-  ~OCREngine() { m_tesseract->End(); }
+  ~OCREngine() { tesseract_->End(); }
 
-  std::string Version() const { return m_tesseract->Version(); }
+  std::string Version() const { return tesseract_->Version(); }
 
   // TODO - Replace int result with something meaningful
   int LoadModel(const std::string& model_data) {
     std::vector<std::string> vars_vec;
     std::vector<std::string> vars_values;
 
-    auto result = m_tesseract->Init(
+    auto result = tesseract_->Init(
         model_data.data(), model_data.size(), "eng", tesseract::OEM_DEFAULT,
         nullptr /* configs */, 0 /* configs_size */, nullptr /* vars_vec */,
         nullptr /* vars_values */, false /* set_only_non_debug_params */,
@@ -32,27 +71,95 @@ class OCREngine {
     return result;
   }
 
-  std::string ExtractText(const std::string& image_data, int width, int height,
-                          int bytes_per_pixel, int bytes_per_line) {
-    // TODO - Sanity check values
-    m_tesseract->SetImage(
+  // TODO - Replace int result with something meaningful
+  int LoadImage(const std::string& image_data, int width, int height,
+                int bytes_per_pixel, int bytes_per_line) {
+    auto min_buffer_len = height * bytes_per_line;
+    if (image_data.size() < min_buffer_len) {
+      return -1;
+    }
+
+    tesseract_->SetImage(
         reinterpret_cast<const unsigned char*>(image_data.data()), width,
         height, bytes_per_pixel, bytes_per_line);
-    m_tesseract->SetRectangle(0, 0, width, height);
+    tesseract_->SetRectangle(0, 0, width, height);
 
-    m_tesseract->Recognize(nullptr /* monitor */);
-    return std::string(m_tesseract->GetUTF8Text());
+    layout_analysis_done_ = false;
+    ocr_done_ = false;
+
+    return 0;
+  }
+
+  std::vector<IntRect> GetBoundingBoxes(TextUnit unit) {
+    if (!layout_analysis_done_) {
+      tesseract_->AnalyseLayout();
+      layout_analysis_done_ = true;
+    }
+    auto iter = unique_from_raw(tesseract_->GetIterator());
+    iter->Begin();
+
+    auto level = iterator_level_from_unit(unit);
+    std::vector<IntRect> boxes;
+    while (iter->Next(level)) {
+      IntRect rect;
+      iter->BoundingBox(level, &rect.left, &rect.top, &rect.right,
+                        &rect.bottom);
+      boxes.push_back(rect);
+    }
+
+    return boxes;
+  }
+
+  std::vector<TextRect> GetText(TextUnit unit) {
+    if (!ocr_done_) {
+      tesseract_->Recognize(nullptr /* monitor */);
+      ocr_done_ = true;
+    }
+    auto iter = unique_from_raw(tesseract_->GetIterator());
+
+    auto level = iterator_level_from_unit(unit);
+
+    std::vector<TextRect> boxes;
+    while (iter->Next(level)) {
+      auto text = string_from_raw(iter->WordNormedUTF8Text());
+      TextRect tr;
+      tr.text = text;
+      iter->BoundingBox(level, &tr.rect.left, &tr.rect.top, &tr.rect.right,
+                        &tr.rect.bottom);
+      boxes.push_back(tr);
+    }
+
+    return boxes;
   }
 
  private:
-  std::unique_ptr<tesseract::TessBaseAPI> m_tesseract;
+  bool layout_analysis_done_ = false;
+  bool ocr_done_ = false;
+  std::unique_ptr<tesseract::TessBaseAPI> tesseract_;
 };
 
-#define OCRLIB_EXPORT extern "C"
-
 EMSCRIPTEN_BINDINGS(ocrlib) {
+  value_object<IntRect>("IntRect")
+      .field("left", &IntRect::left)
+      .field("top", &IntRect::top)
+      .field("right", &IntRect::right)
+      .field("bottom", &IntRect::bottom);
+
+  value_object<TextRect>("TextRect")
+      .field("rect", &TextRect::rect)
+      .field("text", &TextRect::text);
+
   class_<OCREngine>("OCREngine")
       .constructor<>()
       .function("loadModel", &OCREngine::LoadModel)
-      .function("extractText", &OCREngine::ExtractText);
+      .function("loadImage", &OCREngine::LoadImage)
+      .function("getBoundingBoxes", &OCREngine::GetBoundingBoxes)
+      .function("getText", &OCREngine::GetText);
+
+  enum_<TextUnit>("TextUnit")
+      .value("Line", TextUnit::Line)
+      .value("Word", TextUnit::Word);
+
+  register_vector<IntRect>("vector<IntRect>");
+  register_vector<TextRect>("vector<TextRect>");
 }
