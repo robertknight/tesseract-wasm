e73b25f7481e3050c2c57517c9f3f58b2cd6820e
Robert Knight <robertknight@gmail.com>
2022-06-05 15:08:45 +0100

    Convert remaining @type tags to TS syntax

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,7 @@
  * Extract the pixel data from an ImageBitmap.
  */
 export function imageDataFromBitmap(bitmap: ImageBitmap): ImageData {
-  /** @type {HTMLCanvasElement} */
-  let canvas;
+  let canvas: HTMLCanvasElement;
   // @ts-expect-error - OffscreenCanvas API is missing
   if (typeof OffscreenCanvas !== "undefined") {
     // @ts-expect-error - OffscreenCanvas API is missing
@@ -17,8 +16,7 @@ export function imageDataFromBitmap(bitmap: ImageBitmap): ImageData {
     throw new Error("No canvas implementation available");
   }
 
-  const context =
-    /** @type {CanvasRenderingContext2D} */ canvas.getContext("2d");
+  const context = canvas.getContext("2d") as CanvasRenderingContext2D;
   context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height);
   return context.getImageData(0, 0, bitmap.width, bitmap.height);
 }
