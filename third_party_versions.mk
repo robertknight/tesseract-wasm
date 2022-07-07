20fe0d8c62d80152af5e38f098cf5262814b5e89
Robert Knight <robertknight@gmail.com>
2022-07-07 18:24:45 +0100

    Update third_party/ dirs after package version change
    Re-run git fetch and checkout of third party packages if the version
    changes. I might want to look into submodules for this in future.

---
+++ third_party_versions.mk
@@ -0,0 +1,7 @@
+EMSDK_COMMIT=93f21c9ef30bab52de24f9d4ea3f2f377cf6326a
+
+# The Leptonica commit is not a stable release because we need a recent (as of
+# 2022-05-22) commit where WEBP and JPEG support is optional.
+LEPTONICA_COMMIT=ac9a152c6f2ea048bb10660bacfc66181186b825
+
+TESSERACT_COMMIT=5.2.0
