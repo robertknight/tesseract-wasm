7f2a7fa2491350a2d378f897b8369dd3ea161ff6
Robert Knight <robertknight@gmail.com>
2023-01-17 19:09:48 +0000

    Update to Tesseract v5.3.0

--- third_party_versions.mk
+++ third_party_versions.mk
@@ -1,9 +1,8 @@
 # v3.1.16
 EMSDK_COMMIT=961e66c5099e1119dda25f7a8945d155fc561c90
 
-# The Leptonica commit is not a stable release because we need a recent (as of
-# 2022-05-22) commit where WEBP and JPEG support is optional.
-LEPTONICA_COMMIT=ac9a152c6f2ea048bb10660bacfc66181186b825
+# v1.83.0
+LEPTONICA_COMMIT=97b82b09ea2caeb203df4abeb1877eda661e6bfe
 
-# v5.2.0
-TESSERACT_COMMIT=5ad5325a0aa8effc47ca033625b6a51682f82767
+# v5.3.0
+TESSERACT_COMMIT=080da83cc51c4ef8b324a7e03146fe0bd7e0944b
