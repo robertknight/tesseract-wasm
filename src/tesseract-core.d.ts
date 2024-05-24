47be870a85409a2fbc0d7e0fb4d9d7078effcbd6
Matt Leon <108271225+wydengyre@users.noreply.github.com>
2024-05-24 20:02:38 +0200

    tighten tesseract core types (#40)

--- src/tesseract-core.d.ts
+++ src/tesseract-core.d.ts
@@ -1,3 +1,5 @@
 // Type definitions for the Emscripten-generated JS entry point for the
 // WASM file.
-export default function initTesseractCore(options: any = {}): Promise<any>;
+export default function initTesseractCore(
+  options: { wasmBinary?: ArrayBuffer } = {}
+): Promise<unknown>;
