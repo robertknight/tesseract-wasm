359d3049ae41380ee1420764fac6f8c1dabc4894
Matt Leon <matt@wydengyre.com>
2024-06-15 08:22:14 -0400

    tesseract-code-d-ts style
    Preparing for use of biome.

--- src/tesseract-core.d.ts
+++ src/tesseract-core.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for the Emscripten-generated JS entry point for the
 // WASM file.
 export default function initTesseractCore(
-  options: { wasmBinary?: ArrayBuffer } = {}
+  options: { wasmBinary?: ArrayBuffer } = {},
 ): Promise<unknown>;
